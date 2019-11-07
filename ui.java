0a7ace9c3eea473d7b3bdfb3ce3b30f094b94038
Daniel Gamliel <dgamliel@umail.ucsb.edu>
2019-11-07 14:08:25 -0800

    buttons and some new screens

--- ui.java
+++ ui.java
@@ -1,28 +1,63 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-class gui{
+class numPad{
+    GridLayout grid;
 
-    public static void main(String args[]){
+    numPad() {
+        grid = new GridLayout(3, 4, 10, 10);
 
-        //Create Frame
+        for (int i = 0; i < 10; i++) {
+            JButton button = new JButton(Integer.toString(i));
+        }
+    }
+
+    public GridLayout grid(){
+        return this.grid;
+    }
+}
+
+class startScreen {
+    startScreen() {
+        // Create Frame
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1024,720);
+        frame.setSize(1024, 720);
 
-
-        //Define new buttons 
+        // Define new buttons
         JButton tellerButton = new JButton("Teller");
-        JButton userButton   = new JButton("Client");
+        JButton userButton = new JButton("Client");
 
-        int height = 720/3;
+        int height = 720 / 3;
 
-        int left_x  = 1024/3 - 170;    //Makes it look nice
-        int right_x = 1024 - (1024/3); //Makes it look nice lol
+        int left_x = 1024 / 3 - 165; // Makes it look nice
+        int right_x = 1024 - (1024 / 3); // Makes it look nice lol
 
-        //Create the bounds of each button... x,y,width,height
+        // Create the bounds of each button... x,y,width,height
         tellerButton.setBounds(left_x, height, 200, 200);
         userButton.setBounds(right_x, height, 200, 200);
+
+        //Set grid to get pin
+        userButton.addActionListener((ActionListener) new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+
+                JPanel pinPanel = new JPanel();
+                JFormattedTextField pinTextField= new JFormattedTextField();
+
+                pinPanel.add(pinTextField);
+
+                frame.add(pinPanel);
+
+                frame.revalidate();
+
+            }
+        });
+
+
 
         //Add buttons to the GUI
         frame.add(tellerButton);
@@ -31,5 +66,13 @@ class gui{
         frame.setLayout(null);
         frame.setVisible(true);
 
+    }
+
+}
+
+class gui{
+
+    public static void main(String args[]){
+        startScreen s = new startScreen();
     }
 }
