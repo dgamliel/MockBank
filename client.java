dc44c8f540edc7bc62627f7af6013bfb44c6fac3
dgamliel <dgamliel@DESKTOP-UTT8GF2.localdomain>
2019-11-17 13:56:09 -0800

    added function definitions for client class

---
+++ client.java
@@ -0,0 +1,37 @@
+class Client{
+
+    private int my_aid;
+
+    public Client(int aid){
+        this.my_aid = aid;
+    }
+
+    public void wire(){
+        return;
+    }
+    
+    public void purchase(int amt){
+        return;
+    }
+
+    public void withdraw(int amt){
+        return;
+    }
+
+    public void topUp(int amt){
+        return;
+    }
+
+    public void deposit(int amt){
+        return;
+    }
+
+    //TODO: Consider taking in our AID as parameter
+    public void transfer(int src_aid, int dest_aid, double amt){
+        return;
+    }
+
+    public void payFriend(){
+        return;
+    }
+}
